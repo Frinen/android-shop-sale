9d0890705a8e847f0d908cbf7bcc9345782c2639
Frinen <extremalbp@gmail.com>
2018-11-22 00:15:01 +0200

    add menue

--- ShopSale 11.14/app/src/main/java/com/example/shopsale/MainActivity.java
+++ ShopSale 11.14/app/src/main/java/com/example/shopsale/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.shopsale;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
+import android.view.Menu;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -28,6 +29,51 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menue, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_list :
+                {
+                    Bundle arguments = getIntent().getExtras();
+                    try {
+                        selectedItems = (ArrayList<ListItem>) arguments.getSerializable("List");
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    Intent intent = new Intent(this, ItemListActivity.class);
+                    intent.putExtra("List",(Serializable) selectedItems);
+                    startActivity(intent);
+                return true;
+                }
+            case R.id.action_cart:
+                {
+                    Intent intent = new Intent(this, CartActivity.class);
+                    intent.putExtra("List",(Serializable) selectedItems);
+                    startActivity(intent);
+                    return true;
+                }
+            case R.id.action_payment:
+                {
+                    Intent intent = new Intent(this, PaymentActivity.class);
+                    intent.putExtra("List",(Serializable) selectedItems);
+                    startActivity(intent);
+                    return true;
+                }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onItemsList(View view) {
